8459bbdb13401c8c32d9dd0aa1dca0961125e7f2
IMRMT <michaeltimonuli@gmail.com>
2023-12-29 11:41:41 +0700

    baru login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountserviceService } from '../accountservice.service';
 import { AppComponent } from '../app.component';
+import { CerbungserviceService } from '../cerbungservice.service';
 
 @Component({
   selector: 'app-login',
@@ -23,33 +23,54 @@ export class LoginPage implements OnInit {
   enteredPassword: string = "";
 
 
-  constructor(private accountservice:AccountserviceService, private appComponent: AppComponent) { 
-    this.appComponent.showTabs = false
+  constructor(private cerbungservice:CerbungserviceService, private appComponent: AppComponent) {
+    this.enteredNama = localStorage.getItem("app_username") ?? ''
+    this.enteredPassword = localStorage.getItem("app_password") ?? ''
   }
 
   ngOnInit() {
-    this.account = this.accountservice.accounts
+    // this.account = this.cerbungservice.accounts
   }
 
-  CheckValid(names: string, passw: string){
-    if(names === this.account[0].nama){
-      if(passw === this.account[0].password){
+  // CheckValid(names: string, passw: string){
+  //   if(names === this.account[0].nama){
+  //     if(passw === this.account[0].password){
         
-      this.strvalid = "valid"
-      this.route = "/home"
-      this.message = "Login Success"
+  //     this.strvalid = "valid"
+  //     this.route = "/home"
+  //     this.message = "Login Success"
+  //     }
+  //     else if(passw !== this.account[0].password){
+  //     this.strvalid = "invalid"
+  //     this.route = "/login"
+  //     this.message = "Wrong Password"
+  //     }
+  //   }
+  //   else if (names !== this.account[0].nama && passw !== this.account[0].password){ 
+  //     this.strvalid = "invalid"
+  //     this.route = "/login"
+  //     this.message = "Wrong Username and Password"
+  //   }
+  // }
+  login() {
+    this.cerbungservice.login(this.enteredNama, this.enteredPassword).subscribe(
+      (response: any) => {
+        if (response.result === 'success') {
+          alert("success")
+          this.enteredNama = response.fullname
+          localStorage.setItem("app_username", this.enteredNama)
+          localStorage.setItem("app_password", this.enteredPassword)
+        }
+        else {
+          alert(response.message)
+        }
       }
-      else if(passw !== this.account[0].password){
-      this.strvalid = "invalid"
-      this.route = "/login"
-      this.message = "Wrong Password"
-      }
-    }
-    else if (names !== this.account[0].nama && passw !== this.account[0].password){ 
-      this.strvalid = "invalid"
-      this.route = "/login"
-      this.message = "Wrong Username and Password"
-    }
+    );
   }
-
+  logout() {
+    this.enteredNama = ""
+    this.enteredPassword = ""
+    localStorage.removeItem("enteredNama")
+    localStorage.removeItem("enteredPassword")
+  }
 }
